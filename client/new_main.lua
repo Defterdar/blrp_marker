f51bfef3278748d6a984ef4d50fb3f5a8dd9024f
Defterdar <peter@pt-hosting.dk>
2022-05-25 13:28:30 +0000

    more optimization

---
+++ client/new_main.lua
@@ -0,0 +1,77 @@
+-- https://runtime.fivem.net/doc/natives/?_0x28477EC23D892089
+-- locations set in config.lua
+
+local QBCore = exports['qb-core']:GetCoreObject()
+
+local function DrawText3D(x, y, z, text)
+	SetTextScale(0.35, 0.35)
+	SetTextFont(4)
+	SetTextProportional(1)
+	SetTextColour(255, 255, 255, 215)
+	SetTextEntry("STRING")
+	SetTextCentre(true)
+	AddTextComponentString(text)
+	SetDrawOrigin(x,y,z, 0)
+	DrawText(0.0, 0.0)
+	local factor = (string.len(text)) / 370
+	DrawRect(0.0, 0.0+0.0125, 0.017+ factor, 0.03, 0, 0, 0, 75)
+	ClearDrawOrigin()
+end
+
+
+local function markerLoop()
+	CreateThread(function()
+		while listen do
+			wait(1)
+			print("inside zone!")
+		end
+	end)
+end
+
+local listen = false
+
+CreateThread(function()
+	for i = 1, #config, 1 do
+		zone = zones[i]
+		LoopZone[i] = CircleZone:Create(vector3(zone.pos.x,zone.pos.y,zone.pos.z),zone.rad{
+			name = zone.name,
+			debugPoly = zone.debug,
+		})
+
+	end
+	LoopZone[1]:onPlayerInOut(function(isPointInside)
+		if isPointInside then
+			listen = true
+			markerLoop()
+		else
+			listen = false
+		end
+	end)
+end)
+
+
+--Citizen.CreateThread(function()
+--	while true do
+--		Citizen.Wait(1)
+--		for i = 1, #config, 1 do
+--			loc = locations[i]
+  --              
+--			if loc.job == QBCore.Functions.GetPlayerData().job.name or loc.job == "" then
+--
+--				local playerCoord = GetEntityCoords(PlayerPedId(), false)
+  --                      	local locVector = vector3(loc.pos.x, loc.pos.y, loc.pos.z)
+--
+--				dist = #(playerCoord - locVector)
+--
+--				if Vdist2(playerCoord, locVector) < 150 then
+--					DrawMarker(loc.marker,loc.pos.x,loc.pos.y,loc.pos.z-0.75,0.0,0.0,0.0,0.0,0.0,0.0,loc.scale,loc.scale,loc.scale,loc.rgba[1],loc.rgba[2],loc.rgba[3],loc.rgba[4],false,true,2,nil,nil,false)
+--				end
+  --                  
+--				--if Vdist2(playerCoord, locVector) < loc.dist then
+--				if dist < loc.dist then
+--					DrawText3D(loc.pos.x,loc.pos.y,loc.pos.z,loc.text)
+--				end
+--			end
+--		end
+--	end
+--end)
