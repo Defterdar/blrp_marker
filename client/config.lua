b928bf62b621ec312b78e73b9fae4886cfc34448
unknown <peter@pt-hosting.dk>
2022-06-16 15:27:22 +0200

    locations cleanup

--- client/config.lua
+++ client/config.lua
@@ -2,17 +2,17 @@
 
 locations = {
     {
-        pos	= {x= -256.78, y= -982.66, z= 31.22},
+        pos	    = {x= -256.78, y= -982.66, z= 31.22},
         marker	= 23,
         scale 	= 1.0,
         job 	= "police",
-	grade	= 2,
+	    grade	= 2,
         dist	= 4,
         rgba 	= {120, 255, 120, 155},
         text 	= "Tryk ~g~[E]~w~ Debug message"
     },
     {
-        pos	= {x= -262.79, y= -964.18, z= 32.0},
+        pos	    = {x= -262.79, y= -964.18, z= 32.0},
         marker	= 23,
         scale 	= 4.0,
         job 	= "",
@@ -21,7 +21,7 @@ locations = {
         text 	= "~g~[E]~w~ Open Cityhall"
     },
     {
-        pos	= {x= 308.14, y= -595.36, z= 43.29},
+        pos	    = {x= 308.14, y= -595.36, z= 43.29},
         marker	= 1,
         scale 	= 0.0,
         job 	= "",
@@ -30,7 +30,7 @@ locations = {
         text 	= "~g~[E]~w~ Check in"
     },
     {
-        pos	= {x= 309.92, y= -602.95, z= 43.29},
+        pos	    = {x= 309.92, y= -602.95, z= 43.29},
         marker	= 1,
         scale 	= 0.0,
         job 	= "ambulance",
@@ -39,7 +39,7 @@ locations = {
         text 	= "~g~[E]~w~ Armory"
     },
     {
-    	pos	= {x= 317.9, y= -599.6, z= 43.29},
+    	pos	    = {x= 317.9, y= -599.6, z= 43.29},
         marker	= 1,
         scale 	= 0.0,
         job 	= "ambulance",
@@ -48,7 +48,7 @@ locations = {
         text 	= "~g~[E]~w~ Clothing"
     },
     {
-    	pos	= {x= 311.14, y= -599.42, z= 43.29},
+    	pos	    = {x= 311.14, y= -599.42, z= 43.29},
         marker	= 1,
         scale 	= 0.0,
         job 	= "ambulance",
@@ -57,7 +57,7 @@ locations = {
         text 	= "~g~[E]~w~ Toggle duty"
     },
     {
-    	pos	= {x= 298.55, y= -599.63, z= 43.29},
+    	pos	    = {x= 298.55, y= -599.63, z= 43.29},
         marker	= 1,
         scale 	= 0.0,
         job 	= "ambulance",
@@ -66,7 +66,7 @@ locations = {
         text 	= "~g~[E]~w~ Elevator"
     },
     {
-    	pos	= {x= 440.11, y= -975.74, z= 30.69},
+    	pos	    = {x= 440.11, y= -975.74, z= 30.69},
         marker	= 1,
         scale 	= 0.0,
         job 	= "police",
@@ -75,7 +75,7 @@ locations = {
         text 	= "~g~[E]~w~ Toggle duty"
     },
     {
-    	pos	= {x= 452.36, y= -980.17, z= 30.69},
+    	pos	    = {x= 452.36, y= -980.17, z= 30.69},
         marker	= 1,
         scale 	= 0.0,
         job 	= "police",
@@ -84,7 +84,7 @@ locations = {
         text 	= "~g~[E]~w~ Enter Locker"
     },
     {
-    	pos	= {x= 461.87, y= -981.09, z= 30.69},
+    	pos	    = {x= 461.87, y= -981.09, z= 30.69},
         marker	= 1,
         scale 	= 0.0,
         job 	= "police",
@@ -93,7 +93,7 @@ locations = {
         text 	= "~g~[E]~w~ Armory"
     },
     {
-    	pos	= {x= 454.38, y= -988.81, z= 30.69},
+    	pos	    = {x= 454.38, y= -988.81, z= 30.69},
         marker	= 1,
         scale 	= 0.0,
         job 	= "police",
@@ -111,7 +111,7 @@ locations = {
         text 	= "~g~[Evidence 3] "
     },
     {
-    	pos	= {x= 448.13, y= -1018.47, z= 28.56},
+    	pos	    = {x= 448.13, y= -1018.47, z= 28.56},
         marker	= 23,
         scale 	= 1.0,
         job 	= "police",
@@ -120,7 +120,7 @@ locations = {
         text 	= "~g~[Police Garage] "
     },
     {
-    	pos	= {x= 441.99, y= -995.67, z= 30.69},
+    	pos	    = {x= 441.99, y= -995.67, z= 30.69},
         marker	= 23,
         scale 	= 0.0,
         job 	= "police",
@@ -129,7 +129,7 @@ locations = {
         text 	= "~g~[Evidence 1] "
     },
     {
-    	pos	= {x= 451.56, y= -973.23, z= 30.69},
+    	pos	    = {x= 451.56, y= -973.23, z= 30.69},
         marker	= 23,
         scale 	= 0.0,
         job 	= "police",
@@ -138,7 +138,7 @@ locations = {
         text 	= "~g~[Evidence 2] "
     },
     {
-	pos	= {x= 448.98, y= -974.69, z= 30.69},
+	    pos     = {x= 448.98, y= -974.69, z= 30.69},
         marker	= 23,
         scale 	= 0.0,
         job 	= "police",
@@ -147,7 +147,7 @@ locations = {
         text 	= "~g~[E]~w~ Job Management"
     },
     {
-   	pos	= {x= 472.32, y= -1023.6, z= 28.15},
+   	    pos	    = {x= 472.32, y= -1023.6, z= 28.15},
         marker	= 23,
         scale 	= 1.0,
         job 	= "police",
